838d293c9233953be833cc42ee236529dda5e196
satish-venkata-sai-sure <55279619+satish-venkata-sai-sure@users.noreply.github.com>
2019-09-22 23:10:56 +0530

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for callhub project.
+WSGI config for fibo project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'callhub.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fibo.settings')
 
 application = get_wsgi_application()
